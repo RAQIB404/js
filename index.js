9a6dc004f3adbccbd31b67b1bf07e5bea1404f7c
RAQIB404 <raqibkhan2332@gmail.com>
2021-07-24 11:24:45 +0530

    js

---
+++ index.js
@@ -0,0 +1,27 @@
+//primitive data type
+//1.number
+//2.string
+//3.undefined
+//4.null
+//5.boolean
+//6.symbol
+
+var a=6;
+var b="raqib";
+var c;
+var d=null;
+var e=true;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
+console.log(c);//hosting
+var c;
